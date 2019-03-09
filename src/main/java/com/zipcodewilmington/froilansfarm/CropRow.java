47f358d1d5851af9c5361f3bd7e0349e7723b78c
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-03-09 14:40:04 -0500

    starting crop row tests

--- src/main/java/com/zipcodewilmington/froilansfarm/CropRow.java
+++ src/main/java/com/zipcodewilmington/froilansfarm/CropRow.java
@@ -1,12 +1,16 @@
 package com.zipcodewilmington.froilansfarm;
 
+import java.util.ArrayList;
+
 public class CropRow extends Container {
 
     private boolean hasFertilized;
+
     public boolean isHasFertilized() {
         return hasFertilized;
     }
     public boolean setHasFertilized() {
         return false;
     }
+
 }
---
+++ src/test/java/com/zipcodewilmington/froilansfarm/CropRowTest.java
@@ -0,0 +1,11 @@
+package com.zipcodewilmington.froilansfarm;
+
+import org.junit.Test;
+
+public class CropRowTest {
+
+    @Test
+    public void testHasBeenFertilized(){
+
+    }
+}
