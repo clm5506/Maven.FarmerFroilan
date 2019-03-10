b598a6aff475e426d70ab71934d89422f5ec4988
Cristina McClintock <cristinamcclintock@zipcoders-MacBook-Pro.local>
2019-03-10 00:09:27 -0500

    tractor is working now

--- src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
+++ src/test/java/com/zipcodewilmington/froilansfarm/TractorTest.java
@@ -25,4 +25,39 @@ public class TractorTest {
 //        //Then
 //        Assert.assertEquals(expectedSize,actualSize);
 //    }
+
+    @Test
+    public void testHarvest(){
+        //Given
+        Tractor tractor = new Tractor();
+        CropRow cropRow = new CropRow();
+        TomatoPlant tomatoPlant = new TomatoPlant();
+        CornStalk cornStalk = new CornStalk();
+        TomatoPlant tp = new TomatoPlant();
+        cropRow.add(tomatoPlant);
+        cropRow.add(cornStalk);
+        cropRow.add(tp);
+
+
+        //When
+        tractor.harvest(cropRow);
+
+        //Then
+        Assert.assertTrue(cropRow.size() == 0);
+    }
+
+    @Test
+    public void testOperate(){
+        //Given
+        Tractor tractor = new Tractor();
+        Farm testFarm = new Farm();
+
+        //When
+        tractor.operate(testFarm);
+        CropRow actualCropRow = testFarm.getField().getList().get(0);
+
+        //Then
+
+        Assert.assertTrue(actualCropRow.size() == 0);
+    }
 }
